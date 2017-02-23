7cc66353e2834769242a96185c77581b4e205704
Luiz Chagas Jr <luizchagasjr@gmail.com>
2017-02-23 00:30:48 -0600

    db fix

--- server/models/database.js
+++ server/models/database.js
@@ -4,5 +4,8 @@ var config = require('../config/config.json')[env];
 var mydb = new Sequelize(config.database, config.username, config.password, config);
 var Palavra = require('./palavra.js');
 var Voto = require('./votos.js');
+var User = require('./user.js');
 
+Palavra.sync();
+User.sync();
 Voto.sync();
